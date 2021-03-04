3c0ca0197cd6dc52e770b0080453007c385c6269
dongree <dongwoo0307@naver.com>
2021-03-04 21:56:28 +0900

    try except

--- 10820.py
+++ 10820.py
@@ -1,16 +1,16 @@
-while(True):
-    try:
-		s = input()
-	except:
-		break
-	k = [0, 0, 0, 0]
-	for i in s:
-		if i.islower():
-			k[0] += 1
-		elif i.isupper():
-			k[1] += 1
-		elif i.isdigit():
-			k[2] += 1
-		elif i == ' ':
-			k[3] += 1
-	print(k[0], k[1], k[2], k[3])
+# while(True):
+#     try:
+# 		s = input()
+# 	except:
+# 		break
+# 	k = [0, 0, 0, 0]
+# 	for i in s:
+# 		if i.islower():
+# 			k[0] += 1
+# 		elif i.isupper():
+# 			k[1] += 1
+# 		elif i.isdigit():
+# 			k[2] += 1
+# 		elif i == ' ':
+# 			k[3] += 1
+# 	print(k[0], k[1], k[2], k[3])
