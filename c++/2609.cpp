98fd797f5b1d5ec581409da87ba7f7c7d363355a
dongree <dongwoo0307@naver.com>
2022-01-09 20:18:34 +0900

    Euclidean Algorithm

--- c++/2609.cpp
+++ c++/2609.cpp
@@ -1,24 +1,51 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//   int a, b, d = 2;
+//   int lcm, gcd = 1;
+//   cin >> a >> b;
+
+//   while(!(d > a || d > b)){
+//     if(a%d==0 && b%d==0){
+//       a /= d;
+//       b /= d;
+//       gcd *= d;
+//     } else{
+//       d += 1;
+//     }
+//   }
+//   lcm = gcd*a*b;
+
+//   cout << gcd << '\n';
+//   cout << lcm << '\n';
+
+//   return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
-  int a, b, d = 2;
-  int lcm, gcd = 1;
-  cin >> a >> b;
-
-  while(!(d > a || d > b)){
-    if(a%d==0 && b%d==0){
-      a /= d;
-      b /= d;
-      gcd *= d;
-    } else{
-      d += 1;
-    }
+int gcd(int a, int b){
+  int c;
+  while(b!=0){
+    c = a%b;
+    a = b;
+    b = c;
   }
-  lcm = gcd*a*b;
+  return a;
+}
 
-  cout << gcd << '\n';
+int main(){
+  int m, n;
+  cin >> m >> n;
+
+  int g = gcd(m,n);
+  int lcm = m*n/g;
+
+  cout << g << '\n';
   cout << lcm << '\n';
 
   return 0;
