da1a530110f655362731ebb49bb10af51ad751be
dongree <dongwoo0307@naver.com>
2023-02-25 10:05:51 +0900

    divide and conquer

--- c++/2447.cpp
+++ c++/2447.cpp
@@ -1,3 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+// 0-별 찍힘, 1-별 안찍힘
+int arr[6561][6561];
+
+void removeStar(int y, int x, int n){
+  if(n==1) return;
+  int nn=n/3;
+  for(int i=y+nn; i<y+nn*2; i++){
+    for(int j=x+nn; j<x+nn*2; j++){
+      arr[i][j]=1;
+    }
+  }
+
+  removeStar(y,x,nn);
+  removeStar(y+nn,x,nn);
+  removeStar(y,x+nn,nn);
+  removeStar(y+nn*2,x,nn);
+  removeStar(y,x+nn*2,nn);
+  removeStar(y+nn,x+nn*2,nn);
+  removeStar(y+nn*2,x+nn,nn);
+  removeStar(y+nn*2,x+nn*2,nn);
+}
+
+int main(){
+  int n;
+  cin >> n;
+
+  removeStar(0,0,n);
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(arr[i][j]==0) cout << '*';
+      else cout << ' ';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
+
 // #include <iostream>
 
 // using namespace std;
@@ -44,29 +86,29 @@
 //   return 0;
 // }
 
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-void star(int y, int x, int n){
-  if(y/n%3 == 1 && x/n%3 == 1){
-    cout << ' ';
-  }else if(n/3 == 0){
-    cout <<  '*';
-  }else{
-    star(y,x,n/3);
-  }
-}
+// void star(int y, int x, int n){
+//   if(y/n%3 == 1 && x/n%3 == 1){
+//     cout << ' ';
+//   }else if(n/3 == 0){
+//     cout <<  '*';
+//   }else{
+//     star(y,x,n/3);
+//   }
+// }
 
-int main(){
-  int n;
-  cin >> n;
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      star(i,j,n);
-    }
-    cout << '\n';
-  }
+// int main(){
+//   int n;
+//   cin >> n;
+//   for(int i=0; i<n; i++){
+//     for(int j=0; j<n; j++){
+//       star(i,j,n);
+//     }
+//     cout << '\n';
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
