c2f5c35b9958b0c67d7a3749d34a6f80614030cc
dongree <dongwoo0307@naver.com>
2023-12-25 16:25:56 +0900

    bfs

---
+++ js/24445.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [n, m, r] = inputs[0].split(' ').map(Number);
+const adjList = Array(n + 1)
+  .fill()
+  .map(() => []);
+const visited = Array(n + 1).fill(false);
+const result = Array(n).fill(0);
+
+for (let i = 1; i <= m; i++) {
+  const [u, v] = inputs[i].split(' ').map(Number);
+  adjList[u].push(v);
+  adjList[v].push(u);
+}
+
+adjList.forEach((arr) => arr.sort((a, b) => b - a));
+
+const q = [];
+let idx = 0;
+let cnt = 1;
+
+bfs();
+console.log(result.join('\n'));
+
+function bfs() {
+  visited[r] = true;
+  q.push(r);
+  while (q.length !== idx) {
+    let poped = q[idx++];
+    result[poped - 1] = cnt++;
+    adjList[[poped]].forEach((e) => {
+      if (!visited[e]) {
+        visited[e] = true;
+        q.push(e);
+      }
+    });
+  }
+}
