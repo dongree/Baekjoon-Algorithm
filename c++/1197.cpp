5f5ed1c97bbfe00d7899bad2bf0e267807ffa334
dongree <dongwoo0307@naver.com>
2024-02-19 15:34:36 +0900

    kruskal 최적화, prim 알고리즘 추가

--- c++/1197.cpp
+++ c++/1197.cpp
@@ -1,3 +1,4 @@
+// kruskal
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -16,16 +17,16 @@ int getParent(int x) {
 void unionParent(int a, int b) {
   a = getParent(a);
   b = getParent(b);
-  parent[b] = a;
+  if (a < b)
+    parent[b] = a;
+  else
+    parent[a] = b;
 }
 
 bool sameParent(int a, int b) {
   a = getParent(a);
   b = getParent(b);
-  if (a == b)
-    return true;
-  else
-    return false;
+  return a == b;
 }
 
 int main() {
@@ -64,3 +65,56 @@ int main() {
 
   return 0;
 }
+
+// prim
+// #include <iostream>
+// #include <queue>
+
+// using namespace std;
+
+// int V, E;
+// vector<pair<int, int>> v[10001];
+// bool isVisited[10001];
+
+// long long prim() {
+//   long long ans = 0;
+//   priority_queue<pair<int, int>, vector<pair<int, int>>,
+//                  greater<pair<int, int>>>
+//       pq;
+//   pq.push({0, 1});
+
+//   while (!pq.empty()) {
+//     int weight = pq.top().first;
+//     int currNode = pq.top().second;
+//     pq.pop();
+
+//     if (isVisited[currNode]) continue;
+//     isVisited[currNode] = true;
+//     ans += weight;
+
+//     for (int i = 0; i < v[currNode].size(); i++) {
+//       int nextWeight = v[currNode][i].first;
+//       int nextNode = v[currNode][i].second;
+//       if (!isVisited[nextNode]) {
+//         pq.push({nextWeight, nextNode});
+//       }
+//     }
+//   }
+//   return ans;
+// }
+
+// int main() {
+//   freopen("example.txt", "r", stdin);
+//   cin >> V >> E;
+
+//   while (E--) {
+//     int A, B, C;
+//     cin >> A >> B >> C;
+//     v[A].push_back({C, B});
+//     v[B].push_back({C, A});
+//   }
+
+//   cout << prim();
+
+//   return 0;
+// }
